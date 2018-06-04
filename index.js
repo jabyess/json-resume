204cecf56380976386cea45783c5731c72848c72
jabyess <james.byess@gmail.com>
2018-06-04 11:11:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+
+
+fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
+	if (err) {
+			console.error(err)
+	} 
+	else {
+			console.log(data)
+			return data
+	}
+})
+
